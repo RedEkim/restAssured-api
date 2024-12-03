b9fef6b4464c9f655d47a763095368e03f50791c
Michael <mikhail6886@yandex.ru>
2024-12-03 14:25:55 +0500

    "added POST tests to authorization"

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,9 @@
+public class LoginTests {
+    /*
+    1. Make request (POST) to https://reqres.in/api/login
+        with body { "email": "eve.holt@reqres.in", "password": "cityslicka" }
+    2. Get response { "token": "QpwL5tke4Pnpja7X4" }
+    3. Check "token" is "QpwL5tke4Pnpja7X4" and status code 200
+  */
+
+}
--- src/test/java/StatusTests.java
+++ src/test/java/StatusTests.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
 
 public class StatusTests {
@@ -39,6 +40,20 @@ public class StatusTests {
                 .then()
                 .log().all()
                 .body("total", is(20));
+    }
+
+    @Test
+    @DisplayName("API test with Status logs")
+    void checkTotalWithStatusLogs() {
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(20))
+                .body("browsers.chrome", hasKey("101.0"));
     }
 
 }
