c1f63f3bc22ca85aa007ec6442951b7e4b9ec0c8
Michael <mikhail6886@yandex.ru>
2024-12-03 20:25:33 +0500

    "added successfulLoginTest API"

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,3 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
 public class LoginTests {
     /*
     1. Make request (POST) to https://reqres.in/api/login
@@ -6,4 +14,22 @@ public class LoginTests {
     3. Check "token" is "QpwL5tke4Pnpja7X4" and status code 200
   */
 
+    @Test
+    @Tag("Login")
+    @DisplayName("Login test with status code 200")
+    void successfulLoginTest() {
+
+        String authData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
+        given()
+                .body(authData)
+                .contentType(JSON)
+                .log().uri()
+        .when()
+                .post("https://reqres.in/api/login")
+        .then()
+                .log().status()
+                .log().body()
+                .body("token", is("QpwL5tke4Pnpja7X4"))
+                .statusCode(200);
+    }
 }
