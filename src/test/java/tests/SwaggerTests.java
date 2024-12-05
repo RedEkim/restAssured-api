33a97d65cdd9919ae13bbad39894b8814c5fc8f9
Michael <mikhail6886@yandex.ru>
2024-12-05 21:09:34 +0500

    "update comments"

--- src/test/java/tests/SwaggerTests.java
+++ src/test/java/tests/SwaggerTests.java
@@ -258,7 +258,7 @@ public class SwaggerTests extends TestBase {
     User (Operations about user)
     +(POST) /user/createWithList (Creates list of users with given input array)
     +(GET) /user/{username} (Get user by user name)
-    (PUT) /user/{username} (Updated user)
+    +(PUT) /user/{username} (Updated user)
     (DELETE) /user/{username} (Delete user)
     (GET) /user/login (Logs user into the system)
     (GET) /user/logout (Logs out current logged in user session)
