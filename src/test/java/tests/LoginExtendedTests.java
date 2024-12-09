76e82b2b9f663b27eeb1ca606f91e4e71728f2ba
Michael <mikhail6886@yandex.ru>
2024-12-09 12:39:10 +0500

    "updated LoginExtendedTests"

--- src/test/java/tests/LoginExtendedTests.java
+++ src/test/java/tests/LoginExtendedTests.java
@@ -44,8 +44,6 @@ public class LoginExtendedTests {
     @DisplayName("Login test with status code 200")
     void successfulLoginTest() {
 
-//        String authData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
-
         LoginBodyModel authData = new LoginBodyModel();
         authData.setEmail("eve.holt@reqres.in");
         authData.setPassword("cityslicka");
